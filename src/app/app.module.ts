c82bab3e0b5230e0d82e368c8a99a4b88befee83
Johany <59618391+johany03@users.noreply.github.com>
2020-07-20 13:24:09 -0500

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { FormsModule } from '@angular/forms';
+import { ItemsComponent } from './components/items/items.component';
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ItemsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    RouterModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
